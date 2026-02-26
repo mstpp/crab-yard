1f21cb80c194ce59c13f1f150d4456ccdc3bab78
mstpp <stanojlovic.marko+ferris@gmail.com>
2026-02-26 21:49:49 +0100

    chat room over tcp

--- subyards/tokio-yard/chat-room/src/main.rs
+++ subyards/tokio-yard/chat-room/src/main.rs
@@ -1,31 +1,82 @@
-use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+use tokio::sync::{broadcast, mpsc};
+
+#[derive(Debug)]
+struct ChatRoom {
+    tx: broadcast::Sender<String>,
+    rx: mpsc::Receiver<String>,
+}
+
+impl ChatRoom {
+    fn new() -> (Self, mpsc::Sender<String>, broadcast::Sender<String>) {
+        let (tx, rx) = mpsc::channel::<String>(10);
+        let (btx, _) = broadcast::channel::<String>(10);
+        (
+            ChatRoom {
+                tx: btx.clone(),
+                rx: rx,
+            },
+            tx,
+            btx,
+        )
+    }
+
+    async fn run(mut self) {
+        println!("Waitig for messages in the room");
+        while let Some(msg) = self.rx.recv().await {
+            println!("server got msg: {msg}");
+            let _ = self.tx.send(msg);
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     println!("Opened TCP server connection at localhost:7575");
-    let listener = tokio::net::TcpListener::bind("localhost:7575")
-        .await
-        .unwrap(); // TODO err handling
+    let listener = TcpListener::bind("localhost:7575").await.unwrap();
+    let (chat_room, tx, btx) = ChatRoom::new();
+    tokio::spawn(chat_room.run());
 
     // each connection is new tokio async task
     loop {
-        let (tcp_stream, socket_addr) = listener.accept().await.unwrap(); // TODO error handling 
-        tokio::spawn(async move {
-            println!("Connected to {socket_addr:?}");
+        let (tcp_stream, socket_addr) = listener.accept().await.unwrap();
+        let client_tx = tx.clone();
+        let mut clinet_rx = btx.subscribe();
 
-            let reader = BufReader::new(tcp_stream);
-            let mut lines = reader.lines();
+        tokio::spawn(async move {
+            println!("Connected to socket {socket_addr:?}");
+
+            let (reader, mut writer) = tcp_stream.into_split();
+            let mut lines = BufReader::new(reader).lines();
 
             loop {
-                match lines.next_line().await {
-                    Ok(Some(msg)) => println!("got line: {msg}"),
-                    Ok(None) => {
-                        println!("Conn lost {socket_addr}");
-                        break;
+                tokio::select! {
+                    // from tcp client
+                    result = lines.next_line() => {
+                        match result {
+                            Ok(Some(msg)) => {
+                                println!("got line: {msg}");
+                                client_tx.send(format!("{socket_addr}:{msg}")).await.unwrap();
+                            }
+                            Ok(None) => {
+                                println!("Conn lost {socket_addr}");
+                                break;
+                            }
+                            Err(e) => {
+                                println!("conn err {e:?}");
+                                break;
+                            }
+                        }
                     }
-                    Err(e) => {
-                        println!("conn err {e:?}");
-                        break;
+
+                    // from broadcast
+                    result = clinet_rx.recv() => {
+                        if let Ok(msg) = result {
+                            if !msg.as_str().contains(format!("{socket_addr}").as_str()){
+                                let _ = writer.write_all(format!("Broadcast: {msg}\n").as_bytes()).await;
+                            }
+                        }
                     }
                 }
             }
